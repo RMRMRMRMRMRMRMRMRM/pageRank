dc3c3455be51bcfdcb3540ed8825571fc3040aae
RMRMRMRMRMRMRMRMRM <rmradovan.medek@gmail.com>
2025-11-04 11:11:14 +0100

    Added simple pagerank

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def get_adjancency_matrix(filePath):
+    
     with open(file = filePath, mode = 'r', encoding='utf-8', errors='ignore') as f:
         num_websites, num_connections = map(int, f.readline().split())
 
@@ -18,3 +19,44 @@ def get_adjancency_matrix(filePath):
             A[idx - 1, idy - 1] = 1.0   
         
         return id_to_url, A
+
+def pageRank_simple(id_to_url, adj_matrix):
+    num_pages = adj_matrix.shape[1]
+
+    link_matrix = np.zeros_like(adj_matrix)
+
+    outgoing = adj_matrix.sum(axis = 0)
+
+    for src in range(num_pages):
+        if outgoing[src] == 0:
+            continue
+        for dst in range(num_pages):
+            if adj_matrix[src, dst] == 1:
+                link_matrix[src, dst] = 1.0/outgoing[dst]
+
+    x = np.ones(num_pages)/num_pages
+    max_iters = 10000
+    tol = 0.00000001
+
+    eig_vector = power_method(link_matrix, x, max_iters, tol)
+
+    result = {}
+    for page_id, url in id_to_url.items():
+        result[page_id] = [eig_vector[page_id], url]
+
+    return result
+
+def power_method(A, x, max_iters, tol):
+    
+    for _ in range(max_iters):
+        
+        x_next = np.dot(A, x)
+        x_next /= x_next.sum()
+
+        if np.linalg.norm(x_next - x, 1) < tol:
+            break
+        
+        x = x_next
+
+    return x
+
--- main.py
+++ main.py
@@ -2,9 +2,29 @@ import os
 import functions as fn
 import numpy as np
 
-filePath = os.path.join(os.getcwd(), "hollins.dat")
+# filePath = os.path.join(os.getcwd(), "hollins.dat")
 
-id_to_url, A = fn.get_adjancency_matrix(filePath=filePath)
+# id_to_url, A = fn.get_adjancency_matrix(filePath=filePath)
 
-print(f"the dictioanry looks like: {id_to_url}")
-print(f"the matrix has: {np.count_nonzero(A)} nonzero entries")
+# print(f"the dictioanry looks like: {id_to_url}")
+# print(f"the matrix has: {np.count_nonzero(A)} nonzero entries")
+
+# test simple pageRank
+test = np.array([
+    [0, 1, 1, 1],  
+    [0, 0, 1, 1],  
+    [1, 0, 0, 0],  
+    [1, 0, 1, 0]   
+], dtype=float)
+
+id_to_url = {
+    0: "http://site1.com",
+    1: "http://site2.com",
+    2: "http://site3.com",
+    3: "http://site4.com"
+}
+
+ranking = fn.pageRank_simple(id_to_url, test)
+
+print("=== Computed PageRank results ===")
+print(ranking) 
